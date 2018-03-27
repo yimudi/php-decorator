ebf65046bd0083181798d5de874b511a059ed407
yumudi <aphie1900@sina.com>
2018-03-27 14:05:43 +0800

    third commit

--- Ymd/Loader.php
+++ Ymd/Loader.php
@@ -6,6 +6,6 @@ class Loader
 {
     static function autoload($class)
     {
-        require BASEDIR.'/'.str_replace('\\','/',$class).'.php';
+        require BASEDIR . '/' . str_replace('\\', '/', $class) . '.php';
     }
 }
